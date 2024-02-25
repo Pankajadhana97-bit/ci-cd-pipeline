e141a400fbc3e21d092be6e047a895ea1dc86779
Pankaj adhana <geekyblindcoder@gmail.com>
2024-02-26 01:31:19 +0530

    new code changes

--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ app = FastAPI(
     title="FastAPI -(Crud Application with JWT support)",
     description="applying the concepts which we learnt in space of FastAPI",
     contact={
-        "name": "Contact Developer (Pankaj Adhana)",
+        "name": "Pankaj Adhana",
         "email": "pankajadhana97@gmail.com"
     },
     docs_url='/',
