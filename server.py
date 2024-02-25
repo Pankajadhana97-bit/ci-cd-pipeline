595c179183d02bccb6c7c62cb09e1975b1887dd9
Pankaj adhana <geekyblindcoder@gmail.com>
2024-02-25 06:11:16 +0530

    new changes for the files

--- server.py
+++ server.py
@@ -4,17 +4,18 @@ app = FastAPI(
     title="FastAPI -(HashedIn University Revisit)",
     description="Let's create the crud applications which will work with the database of our application which is build over FastAPI",
     contact={
-        "name": "Pankaj Adhana (Backend Developer)", "email": "pankajadhana97@gmail.com"
+        "name": "Pankaj Adhana Dev (Backend Developer)",
+        "email": "pankajadhana97@gmail.com"
     },
     docs_url='/'
 )
 
 
-@app.get("/root")
+@app.get("/api/root")
 async def root():
     return {"message": "Hello World from the amazon web services using dockerhub"}
 
 
-@app.get("/hello/{name}")
+@app.get("/api/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello world {name}  "}
